f04251d027982b38f95f293ae57bbf2aa6fbd532
gbarboza <gbarboza@DF0019661.uniaoquimica.com.br>
2017-10-02 13:10:46 -0300

    Tela de cadastro de plano de ensino criada (incompleta).

--- ProjetoPPC/src/projetoppc/ProjetoPPC.java
+++ ProjetoPPC/src/projetoppc/ProjetoPPC.java
@@ -1,8 +1,13 @@
 package projetoppc;
 
+import javax.swing.JFrame;
+import view.CadastroPlanoDeEnsino;
+
 public class ProjetoPPC {
 
     public static void main(String[] args) {
+        CadastroPlanoDeEnsino tela = new CadastroPlanoDeEnsino(new javax.swing.JFrame(), true);
+        tela.setVisible(true);
     }
     
 }
